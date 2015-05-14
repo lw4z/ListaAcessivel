699c7ad335c9797e51783a496a5c6870ab5f505d
lw4z <ivanvalentimsantos@gmail.com>
2015-05-14 00:29:04 -0300

    Atualizacao da entidade Lista

--- app/src/main/java/mobile/listaacessivel/fafica/listaacessvel/entidades/Lista.java
+++ app/src/main/java/mobile/listaacessivel/fafica/listaacessvel/entidades/Lista.java
@@ -20,8 +20,6 @@ public class Lista {
     private Estabelecimento estabelecimento;
     private List<Produto> produtos;
 
-    //private SimpleDateFormat dmy = new SimpleDateFormat("d/M/y");
-
     public Lista() { }
 
     public Lista(String descricao, String situacao, Cliente cliente, Estabelecimento estabelecimento, List<Produto> produtos) {
@@ -30,20 +28,16 @@ public class Lista {
         this.cliente = cliente;
         this.estabelecimento = estabelecimento;
         this.produtos = produtos;
-        //this.data_criacao = dmy.format(new Date());
     }
 
     public Lista(int id_lista, String descricao, String situacao,
-                 String data_criacao, Cliente cliente,
-                 Estabelecimento estabelecimento, List<Produto> produtos) {
+                 Cliente cliente,Estabelecimento estabelecimento, List<Produto> produtos) {
         this.id_lista = id_lista;
         this.descricao = descricao;
         this.situacao = situacao;
-        this.data_criacao = data_criacao;
         this.cliente = cliente;
         this.estabelecimento = estabelecimento;
         this.produtos = produtos;
-        //this.data_alteracao = dmy.format(new Date());
 
     }
 
