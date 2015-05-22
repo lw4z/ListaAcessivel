3b81382a2b89860630d8922ed18e6139c78abc49
lw4z <ivanvalentimsantos@gmail.com>
2015-05-22 16:56:13 -0300

    Correcao de problema com quantidade de produtos em estabelecimento vazio na tela editar

--- app/src/main/java/mobile/listaacessivel/fafica/listaacessvel/TelaEditarListaPasso2.java
+++ app/src/main/java/mobile/listaacessivel/fafica/listaacessvel/TelaEditarListaPasso2.java
@@ -83,11 +83,11 @@ public class TelaEditarListaPasso2 extends ActionBarActivity {
         produtos = listaProdutosJson.getListaProdutos();
 
         TOTAL_LIST_ITEMS = produtos.size();
-        Produto p = produtos.get(0);
-        Log.e("Metodo TesteGson", p.getDescricao() + ", " + p.getValidade());
-
-
-        Log.i("TAMANHOPRODUTOS",String.valueOf(produtos.size()));
+//        Produto p = produtos.get(0);
+//        Log.e("Metodo TesteGson", p.getDescricao() + ", " + p.getValidade());
+//
+//
+//        Log.i("TAMANHOPRODUTOS",String.valueOf(produtos.size()));
 
         //Carregamento da lista de produtos inicial
         if(produtos.size() > 0) {
